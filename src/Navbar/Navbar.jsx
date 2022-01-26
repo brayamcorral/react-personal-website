289a26f8b1aad1d16c7b62bce1205395b9022802
Brayam Corral Flores <brayamcorral@gmail.com>
2022-01-25 18:19:18 -0800

    navbar toggle

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 import {links, socials} from './data.jsx'
+import { FaBars } from 'react-icons/fa'
 
 function Navbar() {
+
+  const [showToggle, setShowToggle] = useState(true);
+
+  const toggle = () => {
+    setShowToggle(!showToggle);
+  }
+
   return (
       <div className="navbar">
-        <div className="navbar-name"> 
-            Brayam Corral Flores 
-        </div>
-      
-        <ul className="navbar-options"> 
-            {
-              links.map(({id, url, text}) => {
-                return(
-                  <li key = {id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })
-            }
-        </ul>
 
+        <article className="navbar-header">
+          <div className="navbar-name"> 
+              Brayam Corral Flores 
+          </div>
+          
+          <div className="navbar-toggle"> 
+            <FaBars onClick={() => toggle()}/>
+          </div>
+        </article>
+        
+        {showToggle && 
+          <ul className="navbar-options"> 
+              {
+                links.map(({id, url, text}) => {
+                  return(
+                    <li key = {id}>
+                      <a href={url}>{text}</a>
+                    </li>
+                  );
+                })
+              }
+          </ul>
+        }
         <ul className="navbar-icons"> 
             {
               socials.map(({id, url, icon}) => {
