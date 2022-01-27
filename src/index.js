1990d104e695b8024f6377ca1e7467457915a989
Brayam Corral Flores <brayamcorral@gmail.com>
2022-01-26 19:42:35 -0800

    added router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './scenes/Home/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Contact from './scenes/Contact/Contact.jsx';
+import Home from './scenes/Home/Home.jsx';
+import Projects from './scenes/Projects/Projects.jsx';
+import Error from './scenes/Error/Error.jsx';
+import Navbar from './components/Navbar/Navbar.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Navbar />
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/Contact'>
+          <Contact />
+        </Route>
+        <Route path='/Projects'>
+          <Projects />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/scenes/Home/Home.jsx
+++ src/scenes/Home/Home.jsx
@@ -1,9 +1,8 @@
 import './Home.css';
-import Navbar from '../../components/Navbar/Navbar.jsx'
+
 function App() {
   return (
     <>
-      <Navbar />
       <div className="under-construction">
         <h1> UNDER CONSTRUCTION! 2022</h1> 
       </div>
